79bc5c5745ff53b1bc6545e24a1156f9440f2fec
mattGallina <matt.gallina01@gmail.com>
2026-02-25 16:31:33 +0100

    gyro logger method, dart vm not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,60 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
 import 'firebase_options.dart';
 import 'screens/auth_screen.dart';
 import 'screens/chat_list_screen.dart';
 
-void main() async {
+/// LOGGER GIROSCOPIO (servizio semplice)
+class SensorLogger {
+  StreamSubscription<GyroscopeEvent>? _sub;
+  DateTime _last = DateTime.fromMillisecondsSinceEpoch(0);
+
+  void start({int hz = 25}) {
+    final minIntervalMs = (1000 / hz).round();
+
+    _sub = gyroscopeEvents.listen((e) {
+      final now = DateTime.now();
+      if (now.difference(_last).inMilliseconds < minIntervalMs) return;
+      _last = now;
+
+      debugPrint(
+        'GYRO ts=${now.toIso8601String()} '
+        'x=${e.x.toStringAsFixed(4)} '
+        'y=${e.y.toStringAsFixed(4)} '
+        'z=${e.z.toStringAsFixed(4)} rad/s',
+      );
+    }, onError: (err) {
+      debugPrint('Gyro error: $err');
+    });
+  }
+
+  Future<void> stop() async {
+    await _sub?.cancel();
+    _sub = null;
+  }
+}
+
+/// Istanza globale del logger
+final SensorLogger sensorLogger = SensorLogger();
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
+  /// Avvio logger giroscopio
+  sensorLogger.start(hz: 25);
+
   runApp(const MyApp());
 }
 
+/// APP PRINCIPALE
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -28,7 +70,7 @@ class MyApp extends StatelessWidget {
         splashColor: Colors.transparent,
         highlightColor: Colors.transparent,
         scaffoldBackgroundColor: Colors.transparent,
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           bodyLarge: TextStyle(color: Colors.white),
           bodyMedium: TextStyle(color: Colors.white),
           bodySmall: TextStyle(color: Colors.white),
@@ -37,7 +79,7 @@ class MyApp extends StatelessWidget {
           titleSmall: TextStyle(color: Colors.white),
         ),
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFFB77EF1), // blu Telegram
+          seedColor: const Color(0xFFB77EF1),
           brightness: Brightness.dark,
         ),
         useMaterial3: true,
