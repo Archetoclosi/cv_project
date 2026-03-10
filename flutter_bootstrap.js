a3919bd3ca288e3e323957944c43f03313b8e1e5
mattGallina <matt.gallina01@gmail.com>
2026-03-10 13:26:57 +0100

    Built web
    Branch: master
    Commit: fa03d51dba86dd892cee2084d2ceb8dc3c446775 (dirty)
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1258469915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4285145122" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
